209c5c7d1531c5aa9217d5c92188ce0300aa7594
Fastares <104374126+Fastares@users.noreply.github.com>
2026-04-27 13:55:12 -0400

    calender
    now with labels and days

---
+++ projectf/src/main/java/com/example/projectf/Monthlycalender.java
@@ -0,0 +1,244 @@
+package com.example.projectf;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Monthlycalender extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Pane layout = new Pane();
+        Scene scene = new Scene(layout, 650, 400);
+
+        // calender layout
+        Rectwboarder plate = new Rectwboarder(480, 350);
+        plate.setLayoutY(40);
+        plate.setLayoutX(35);
+        layout.getChildren().addAll(plate.getBoarder(), plate.getFillw());
+
+        // the dividers that give spaces
+        Yaxisgrid(37, 65,500, 65, 55, 6, layout);
+        Xaxisgrid(102.5, 42, 102.5, 388, 65, 7, layout);
+
+        //days of the month for layout
+        daysinweek(64, 45, 65, layout);
+
+        //upandcoming layout
+        Rectwboarder sidedish = new Rectwboarder(120, 350);
+        sidedish.setLayoutX(500);
+        sidedish.setLayoutY(40);
+
+        //add the sidebar and calender to the side
+        layout.getChildren().addAll(sidedish.getBoarder(), sidedish.getFillw());
+
+        // days
+        Daysinmonth slide = new Daysinmonth(31, "M", layout, 40, 48);
+
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    void Yaxisgrid(double startX, double startY, double endX, double endY, double space, int num, Pane layout) {
+        Line[] startpl = new Line[num];
+        int i = 0;
+        while (i < num) {
+            if (i == 0) {
+                startpl[i] = new Line(startX, startY, endX, endY);
+                layout.getChildren().add(startpl[i]);
+                i++;
+                continue;
+            }
+            startpl[i] = new Line(startX, startpl[i - 1].getStartY() + space, endX, startpl[i - 1].getEndY() + space);
+            layout.getChildren().add(startpl[i]);
+            i++;
+        }
+    }
+    void Xaxisgrid(double startX, double startY, double endX, double endY, double space, int num, Pane layout) {
+        Line[] endpl = new Line[num];
+        int i = 0;
+        while (i < num) {
+            if (i == 0) {
+                endpl[i] = new Line(startX, startY, endX, endY);
+                layout.getChildren().add(endpl[i]);
+                i++;
+                continue;
+            }
+            endpl[i] = new Line(endpl[i - 1].getStartX() + space, startY, endpl[i - 1].getEndX() + space, endY);
+            layout.getChildren().add(endpl[i]);
+            i++;
+        }
+    }
+    void daysinweek(double X, double Y, double divider, Pane layout) {
+        Label Sunday = new Label("Su");
+        Sunday.setLayoutX(X);
+        Sunday.setLayoutY(Y);
+
+        Label Monday = new Label("M");
+        Monday.setLayoutX(Sunday.getLayoutX() + divider);
+        Monday.setLayoutY(Y);
+
+        Label Tuesday = new Label("Tu");
+        Tuesday.setLayoutX(Monday.getLayoutX() + divider);
+        Tuesday.setLayoutY(Y);
+
+        Label Wesnesday = new Label("W");
+        Wesnesday.setLayoutX(Tuesday.getLayoutX() + divider);
+        Wesnesday.setLayoutY(Y);
+
+
+        Label Thursday = new Label("Th");
+        Thursday.setLayoutX(Wesnesday.getLayoutX() + divider);
+        Thursday.setLayoutY(Y);
+
+        Label Friday = new Label("F");
+        Friday.setLayoutX(Thursday.getLayoutX() + divider);
+        Friday.setLayoutY(Y);
+
+
+        Label Saturday = new Label("Sa");
+        Saturday.setLayoutX(Friday.getLayoutX() + divider);
+        Saturday.setLayoutY(Y);
+
+        layout.getChildren().addAll(Sunday , Monday, Tuesday, Wesnesday, Thursday, Friday, Saturday);
+    }
+}
+// this is for the event bar and calender area
+class Rectwboarder {
+    private Rectangle boarder;
+    private Rectangle fillw;
+    // constructor
+    public Rectwboarder(double width, double height) {
+        boarder = new Rectangle(width, height);
+        fillw = new Rectangle(boarder.getWidth() - 4 , boarder.getHeight() - 4);
+        fillw.setLayoutX(2);
+        fillw.setLayoutY(2);
+
+        SetFillarea(Color.WHITE);
+        SetFillbound(Color.BLACK);
+
+    }
+    // rectangle grab (just incase)
+    public Rectangle getBoarder() { return boarder; }
+    public Rectangle getFillw() { return fillw; }
+    // color
+    public void SetFillarea (Paint value) {fillw.setFill(value);}
+    public void SetFillbound (Paint value) {boarder.setFill(value);}
+    public void SetFill(Paint value) {
+        fillw.setFill(value);
+        boarder.setFill(value);
+    }
+    // x pos
+    public void setLayoutX(double Xmark) {
+        boarder.setLayoutX(Xmark);
+        fillw.setLayoutX((Xmark + (boarder.getWidth() - fillw.getWidth()) / 2));
+    }
+    public double getLayoutX() { return boarder.getLayoutX(); }
+    // y pos
+    public void setLayoutY (double Ymark) {
+        boarder.setLayoutY(Ymark);
+        fillw.setLayoutY(Ymark + (boarder.getHeight() - fillw.getHeight()) / 2);
+    }
+    public double getLayoutY() {return boarder.getLayoutY();}
+
+}
+//event bar wip but it takes the nearest hopefully 6 events and makes them into a list from clost to far
+class Eventbar extends Rectwboarder {
+    private int eventcount = 6;
+    private int current = 0;
+    public Eventbar(double width, double height) {
+        super(width, height);
+    }
+}
+// this makes the sorting system of which day it is for example 1 beings up the events of one
+class Day {
+    private Label test;
+    private Button side;
+
+    public Day(int count, Pane hold, double X, double Y) {
+        test = new Label("" + count);
+        test.setPrefSize(50, 50);
+        test.setLayoutX(X);
+        test.setLayoutY(Y);
+        side = new Button("event: 0");
+        side.setLayoutX(X - 3);
+        side.setLayoutY(Y + 45.5);
+        side.setPrefSize(65, 25);
+
+        hold.getChildren().addAll(test, side);
+    }
+
+    public void setLayoutX(double X) {
+        test.setLayoutX(X);
+        side.setLayoutX(X - 3);
+    }
+
+    public void setLayoutY(double Y) {
+        test.setLayoutY(Y);
+        side.setLayoutY(Y + 45.5);
+    }
+
+    public void addthis(Pane layout) {
+        layout.getChildren().addAll(test, side);
+    }
+
+
+}
+// used for the calender so it can spam the number of days
+class Daysinmonth {
+    private Day[] days;
+    private int Sx = 65;
+    private int Sy = 55;
+    // day is total amount of days, press is the starting day, layout is the pane needed, px is starting x and py is starting y
+    public Daysinmonth(int day, String press, Pane layout,  double pX, double pY) {
+        days = new Day[day];
+        int i = 0;
+        int counter = 0;
+        int start = montonum(press);
+        int temp = start + day;
+        double hori = pX;
+        double veri = pY;
+
+        while (i != start) {
+            hori = hori + Sx;
+            counter++;
+            i++;
+        }
+        i = 0;
+        while (i != day) {
+            if (counter < 7) {
+                days[i] = new Day(i + 1, layout, hori, veri);
+                hori = hori + Sx;
+            } else {
+                counter = 0;
+                veri = veri + Sy;
+                hori = pX;
+                continue;
+            }
+            counter++;
+            i++;
+        }
+    }
+
+    private int montonum(String press) {
+        switch (press){
+            case "Su": return 0;
+            case "M": return 1;
+            case "Tu": return 2;
+            case "W" : return 3;
+            case "Th": return 4;
+            case "F" : return 5;
+            case "Sa": return 6;
+        }
+        return 0;
+    }
+}
