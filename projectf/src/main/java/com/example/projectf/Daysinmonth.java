f14883f69f3eb41be7ed1dc0a7323d280e887dad
josiedriscoll <77085967+josiedriscoll@users.noreply.github.com>
2026-05-06 23:31:56 -0400

    Update Daysinmonth.java

--- projectf/src/main/java/com/example/projectf/Daysinmonth.java
+++ projectf/src/main/java/com/example/projectf/Daysinmonth.java
@@ -4,16 +4,17 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 // used for the calender so it can spam the number of days
-public class Daysinmonth {
+class Daysinmonth {
     private Day[] days;
     private int Sx = 65;
     private int Sy = 55;
     // day is total amount of days, press is the starting day, layout is the pane needed, px is starting x and py is starting y
-    public Daysinmonth(int day, String press, Pane layout, double pX, double pY, Stage stage) {
+    public Daysinmonth(int day, int startOffset, Pane layout, double pX, double pY, Stage stage,
+                       Label sidebarTitle, ListView<String> sidebarList) {
         days = new Day[day];
         int i = 0;
         int counter = 0;
-        int start = montonum(press) - 1;
+        int start = startOffset;
         double hori = pX;
         double veri = pY;
         while (i != start) {
@@ -24,7 +25,7 @@ public class Daysinmonth {
         i = 0;
         while (i != day) {
             if (counter < 7) {
-                days[i] = new Day(i + 1, layout, hori, veri);
+                days[i] = new Day(i + 1, layout, hori, veri, sidebarTitle, sidebarList);
                 days[i].center = stage;
                 hori = hori + Sx;
             } else {
@@ -39,22 +40,15 @@ public class Daysinmonth {
     }
 
     private int montonum(String press) {
-         switch (press) {
-             case "Su":
-                 return 1;
-            case "M":
-                return 2;
-            case "Tu":
-                return 3;
-            case "W":
-                return 4;
-            case "Th":
-                return 5;
-            case "F":
-                return 6;
-            case "Sa":
-                return 7;
+        switch (press){
+            case "Su": return 1;
+            case "M": return 2;
+            case "Tu": return 3;
+            case "W" : return 4;
+            case "Th": return 5;
+            case "F" : return 6;
+            case "Sa": return 7;
         }
-         return 1;
+        return 0;
     }
 }
